c7efb9b7aaef65cac0ddf98fe15d044e89ba6c26
marikadiblasio <marbla.albram@gmail.com>
2023-05-24 16:33:04 +0200

    create trainseeder and traicontroller, print data in home

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Models\Train;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,8 +15,20 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 10; $i++){
+            $newTrain = new Train();
+            $newTrain->company = $faker->randomElement(['Italo', 'Trenitalia', 'Trenord']);
+            $newTrain->departure_station= $faker->randomElement(['Milano', 'Bologna', 'Firenze', 'Torino', 'Napoli', 'Salerno', 'Genova', 'Roma', 'Pisa', 'Venezia']);
+            $newTrain->arrival_station= $faker->randomElement(['Milano', 'Bologna', 'Ancona', 'Torino', 'Napoli', 'Brescia', 'Genova', 'Roma', 'Pisa', 'Venezia']);
+            $newTrain->departure_time= $faker->time();
+            $newTrain->arrival_time= $faker->time();
+            $newTrain->train_code=$faker->numerify('######');
+            $newTrain->carriage_num=$faker->numberBetween(0, 10);
+            $newTrain->onTime= $faker->boolean();
+            $newTrain->cancelled= $faker->boolean();
+            $newTrain->save();
+        }
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Train</h1>
+<main>
+    <h1>I treni di oggi:</h1>
+    <div class="container">
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Stazione di Partenza</th>
+                <th scope="col">Stazione di Arrivo</th>
+                <th scope="col">Orario di Partenza</th>
+                <th scope="col">Orario di Arrivo</th>
+                <th scope="col">Compagnia</th>
+                <th scope="col">Codice treno</th>
+                <th scope="col">Carrozze</th>
+                <th scope="col">In orario</th>
+                <th scope="col">Cancellato</th>
+              </tr>
+            </thead>
+            @foreach ($trains as $train)
+            <tbody>
+              <tr>
+                <td scope="row">{{$train->departure_station}}</td>
+                <td>{{$train->arrival_station}}</td>
+                <td>{{substr($train->departure_time, 0,-3)}}</td>
+                <td>{{substr($train->arrival_time, 0,-3)}}</td>
+                <td>{{$train->company}}</td>
+                <td>{{$train->train_code}}</td>
+                <td>{{$train->carriage_num}}</td>
+                <td>{{$train->onTime}}</td>
+                <td>{{$train->cancelled}}</td>
+              </tr>
+            </tbody>
+            @endforeach
+          </table>
+    </div>
+</main>
 @endsection
